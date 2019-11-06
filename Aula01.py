1d954283fe07fb54759e916f969ef36c13e976a3
Richarde <lucasoaresleite@gmail.com>
2019-11-05 21:14:52 -0300

    Enviando Aula01

--- Aula01.py
+++ Aula01.py
@@ -1,10 +1,13 @@
+# Serve para indicar os valores a ser somado.
 a = 1
 b = 5
 soma = a + b
-
+# Serve para mostrar o resultado que a soma e maoir.
 if(soma > a):
     print("Soma maior que A")
+# Serve para mostra que a soma e Igual.
 elif(soma == a):
     print("Soma igual a A")
+# Serve para mostrar que a soma e menor.
 else:
     print("A soma menor que A")
