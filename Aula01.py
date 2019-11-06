cf20d6140365897ee4b574605a002edd801e07f8
Richarde <lucasoaresleite@gmail.com>
2019-11-05 21:45:06 -0300

    Atualizando Aula01

--- Aula01.py
+++ Aula01.py
@@ -2,6 +2,7 @@
 a = 1
 b = 5
 soma = a + b
+pessoaNome = "Fulano"
 # Serve para mostrar o resultado que a soma e maoir.
 if(soma > a):
     print("Soma maior que A")
@@ -11,3 +12,10 @@ elif(soma == a):
 # Serve para mostrar que a soma e menor.
 else:
     print("A soma menor que A")
+
+print("A primeira letra do nome é: "+pessoaNome[0])
+#Vai mostrar a primeira letra do Nome.
+print("A Segunda letra do nome é: "+pessoaNome[3])
+#Vai mostra a Letra do meio do nome.
+print("A Terceira letra do nome é: "+pessoaNome[5])
+#Vai mostra a ultima letra do nome.
